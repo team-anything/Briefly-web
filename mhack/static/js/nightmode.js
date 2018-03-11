4816088194f44b606ad87fb4e09bacfc0c36064b
Neel Shah <neelshah.1998@gmail.com>
2018-03-11 11:30:20 +0530

    cookies added

--- mhack/static/js/nightmode.js
+++ mhack/static/js/nightmode.js
@@ -1,25 +1,34 @@
 var root = document.documentElement;
 var nightmode = document.getElementById("nightmode");
 var nightmodeDiv = document.getElementsByClassName("nightmode")[0];
-var active = true;
 
-nightmodeDiv.addEventListener("click", function(){
-	if(nightmode.checked){
-		active=true;
-		root.style.setProperty('--color-primary', '#f56a6a');
-		root.style.setProperty('--color-white', '#222');
-		root.style.setProperty('--color-white-dark', '#555');
-		root.style.setProperty('--color-black', '#fff');
+nightmodeDiv.addEventListener("click", function(e){
+	if(e.target.nodeName=="INPUT"){
+		if(nightmode.checked){
+			Cookies.set('nightmode',true);
+			console.log(Cookies.get('nightmode'),e.target.nodeName);
+			root.style.setProperty('--color-primary', '#f56a6a');
+			root.style.setProperty('--color-white', '#222');
+			root.style.setProperty('--color-white-dark', '#555');
+			root.style.setProperty('--color-black', '#fff');
+		}
+		else {
+			Cookies.set('nightmode',false);
+			console.log(Cookies.get('nightmode'),e.target.nodeName);
+			root.style.setProperty('--color-primary', '#f56a6a');
+			root.style.setProperty('--color-white', '#fff');
+			root.style.setProperty('--color-white-dark', '#ddd');
+			root.style.setProperty('--color-black', '#222');
+		};
 	}
-	else {
-		active=false;
-		root.style.setProperty('--color-primary', '#f56a6a');
-		root.style.setProperty('--color-white', '#fff');
-		root.style.setProperty('--color-white-dark', '#ddd');
-		root.style.setProperty('--color-black', '#222');
-	};
 });
-
+function preLoad(){
+	console.log(Cookies.get('nightmode'));
+	if(Cookies.get('nightmode')==="true"){
+		console.log("xxxxxx");
+		nightmode.click();
+	}
+};
 // function preLoad(){
 // 	if(active){
 // 		nightmode.checked=true;
@@ -35,4 +44,4 @@ nightmodeDiv.addEventListener("click", function(){
 // 		root.style.setProperty('--color-white-dark', '#ddd');
 // 		root.style.setProperty('--color-black', '#222');	}
 // }
-// window.onpaint = preLoad();
+window.onload = preLoad();
